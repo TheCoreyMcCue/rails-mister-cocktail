edcd01a1cc90fb9dd5283c918ce94a289e526da9
Corey McCue <coreysmccue@gmail.com>
2021-02-18 16:12:02 +0100

    complete app functionality

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,24 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktails_path, notice: 'dose was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
